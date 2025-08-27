7e45f974335d1778476c667694125163a185f7e9
jbinyim <jbinyim12@naver.com>
2025-08-28 02:07:19 +0900

    feat: 메인 홈 구현

---
+++ src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react";
+import HomeContent from "../components/home/HomeContent";
+
+export default function Home() {
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const x = (e.clientX - window.innerWidth / 2) / window.innerWidth;
+      const y = (e.clientY - window.innerHeight / 2) / window.innerHeight;
+
+      setMousePosition({ x, y });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => window.removeEventListener("mousemove", handleMouseMove);
+  }, []);
+
+  return (
+    <div>
+      <div className="sticky top-0 left-0 z-0 h-screen">
+        <div className="min-h-screen relative overflow-hidden">
+          <div
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat transition-transform duration-100 ease-out"
+            style={{
+              backgroundImage: "url(/hero-bg.jpg)",
+              transform: `translate(${mousePosition.x * 20}px, ${
+                mousePosition.y * 20
+              }px) scale(1.05)`,
+            }}
+          />
+          <HomeContent />
+        </div>
+      </div>
+      <div className="min-h-screen bg-white relative z-10">zz</div>
+    </div>
+  );
+}
