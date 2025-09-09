4638e76c8e45f715340ca95be9fd265ab8c61141
jbinyim <jbinyim12@naver.com>
2025-09-10 03:01:04 +0900

    feat: 프로젝트 구현중

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,21 +1,22 @@
 import { useState } from "react";
 import { projects } from "../utils/project";
+import { Link } from "react-router-dom";
 
 export default function Project() {
   const [selectedProject, setSelectedProject] = useState(projects[0]);
 
   return (
     <main className="bg-[#242424] w-full min-h-screen h-full">
-      <section className="mx-auto flex gap-8">
+      <section className=" mx-auto flex flex-col lg:flex-row gap-8">
         {/* 왼쪽 큰 프로젝트 영역 */}
         <article
-          className="w-1/2 h-[calc(100vh-74px)] flex flex-col sticky top-18.5 left-0"
+          className="lg:w-1/2 h-[calc(100vh-74px)] flex flex-col lg:sticky top-18.5 left-0"
           style={{ backgroundColor: `${selectedProject.color.bg}` }}
         >
           <img
-            src={selectedProject.img[0]}
+            src={selectedProject.img[1]}
             alt="프로젝트 썸네일"
-            className="object-contain w-[500px] h-[800px] mx-auto"
+            className="object-contain w-[500px] h-[500px] mx-auto"
           />
 
           <div
@@ -47,6 +48,21 @@ export default function Project() {
               <div className="text-[#afb6bc] mt-4">
                 <p className="text-20-bold line-clamp-2">{project.oneLine}</p>
                 <p className="text-10-normal mt-6">{project.date}</p>
+              </div>
+              <div className="flex items-center justify-between gap-2 text-white">
+                <Link
+                  to={`/project/${project.id}`}
+                  className="border border-white w-1/2 rounded-sm text-center py-2"
+                >
+                  상세 보기
+                </Link>
+                <Link
+                  to={project.url}
+                  target="_blank"
+                  className="border border-white w-1/2 rounded-sm text-center py-2"
+                >
+                  사이트 바로가기
+                </Link>
               </div>
             </div>
           ))}
